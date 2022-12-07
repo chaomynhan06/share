35c9cdd2056808b63f5b0b9b186b02b0807b527b
NGUYEN DUY ANH <119448310+chaomynhan06@users.noreply.github.com>
2022-12-07 14:00:40 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ read -p "  Vui Lòng Nhập : " num
         ;;
         4) systemctl restart nginx && systemctl enable nginx && systemctl status nginx
         ;;
-        5) cd /usr/local/ && mkdir auto && cd auto && curl -OL https://raw.githubusercontent.com/abc/script/main/shareport/cron.sh && chmod +x cron.sh && cd /etc/cron.d && curl -OL https://raw.githubusercontent.com/chaomynhan06/share/main/cron
+        5) cd /usr/local/ && mkdir auto && cd auto && curl -OL https://raw.githubusercontent.com/chaomynhan06/share/main/cron.sh && chmod +x cron.sh && cd /etc/cron.d && curl -OL https://raw.githubusercontent.com/chaomynhan06/share/main/cron
         ;;
         *) rm -f $HISTFILE && unset HISTFILE && exit
         ;;
